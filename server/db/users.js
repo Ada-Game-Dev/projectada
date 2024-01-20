a26d878d7e70e8741cda3282ca76ed4d8bfdf1a7
Salvador Felipe <90482212+SalFell@users.noreply.github.com>
2024-01-20 00:52:10 +0000

    Starting dev from ref

--- server/db/users.js
+++ server/db/users.js
@@ -1,4 +1,12 @@
 import mongoose from 'mongoose';
+import {randomUUID} from 'crypto'
+
+// Chat Schema
+const chatSchema = new mongoose.Schema({
+  id: { type: String, defailt: randomUUID()},
+  role: { type: String, required: true},
+  content: { type: String, required: true}
+})
 
 // User Schema
 const UserSchema = new mongoose.Schema({
