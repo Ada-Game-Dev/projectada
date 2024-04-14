6fae9107648fa9492c02f5e16e81c5c1a9849782
Salvador Felipe <sfel@csu.fullerton.edu>
2024-04-14 15:41:47 -0700

    Fixed Signup not posting
    port was listening to 8080 instead of 5000.
    line 18 in app.ts in backend was updated to correct path of /api/v1

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,7 +2,7 @@ import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
 //connections and listeneres
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 connectToDatabase()
   .then(() => {
     app.listen(PORT, () =>
